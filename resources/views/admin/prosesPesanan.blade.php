6bb40598d0d9d0d106673274c3aa3dae03ccb028
Rake Wangsa <rakekw28@gmail.com>
2023-04-27 23:53:08 +0700

    commit 26

--- resources/views/admin/prosesPesanan.blade.php
+++ resources/views/admin/prosesPesanan.blade.php
@@ -42,9 +42,9 @@
                   <option value="Ditolak">Ditolak</option>
                 </select>
               </div>
-              <div class="col-md-12"> <label for="supir" class="form-label">Supir :</label> <input type="text" class="form-control" id="supir" name="supir" value="{{ old('supir') }}" required></div>
-              <div class="col-md-12"> <label for="harga" class="form-label">Kontak Supir :</label> <input type="text" class="form-control" id="kontaksupir" name="kontaksupir" value="{{ old('kontaksupir') }}" required></div>
-              <div class="col-md-12"> <label for="harga" class="form-label">Harga :</label> <input type="text" class="form-control" id="harga" name="harga" value="{{ old('harga') }}" required></div>
+              <div class="col-md-12" id="cek1"> <label for="supir" class="form-label">Supir :</label> <input type="text" class="form-control" id="supir" name="supir" value="{{ old('supir') }}" required></div>
+              <div class="col-md-12" id="cek2"> <label for="harga" class="form-label">Kontak Supir :</label> <input type="text" class="form-control" id="kontaksupir" name="kontaksupir" value="{{ old('kontaksupir') }}" required></div>
+              <div class="col-md-12" id="cek3"> <label for="harga" class="form-label">Harga :</label> <input type="text" class="form-control" id="harga" name="harga" value="{{ old('harga') }}" required></div>
               <div class="col-md-12"> <label for="keterangan2" class="form-label">Keterangan :</label> <input type="text" class="form-control" id="keterangan2" name="keterangan2" value="{{ old('keterangan2') }}" required></div>
               <div class="text-center mb-5 mt-4"> <button type="submit" class="btn btn-primary">Submit</button> <button type="reset" class="btn btn-secondary">Reset</button></div>
           </form>
@@ -52,4 +52,33 @@
       </div>
 </div>
   
+<script>
+   const statusSelect = document.querySelector('#status');
+   const tanggalPulangInput = document.querySelector('#cek1');
+   const tanggalPulangInput = document.querySelector('#cek2');
+   const tanggalPulangInput = document.querySelector('#cek3');
+   
+   // Check if the selected option is "Pulang-Pergi" on page load
+   if (statusSelect.value === 'Diterima') {
+     tanggalPulangInput.style.display = 'block';
+     tanggalPulangInput.querySelector('input').setAttribute('required', 'required');
+   } else {
+     tanggalPulangInput.style.display = 'none';
+     tanggalPulangInput.querySelector('input').removeAttribute('required');
+     tanggalPulangInput.querySelector('input').value = '';
+   }
+   
+   // Add event listener to jenisSelect
+   statusSelect.addEventListener('change', function() {
+     // Check if the selected option is "Pulang-Pergi"
+     if (this.value === 'Diterima') {
+       tanggalPulangInput.style.display = 'block';
+       tanggalPulangInput.querySelector('input').setAttribute('required', 'required');
+     } else {
+       tanggalPulangInput.style.display = 'none';
+       tanggalPulangInput.querySelector('input').removeAttribute('required');
+       tanggalPulangInput.querySelector('input').value = '';
+     }
+   });
+   </script>
 @endsection
