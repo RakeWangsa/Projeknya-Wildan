e01f29b5ba85e124b968de6f6e3e05989610e2c0
Rake Wangsa <rakekw28@gmail.com>
2023-04-28 23:27:49 +0700

    commit 27

--- resources/views/user/pesananSaya.blade.php
+++ resources/views/user/pesananSaya.blade.php
@@ -52,7 +52,75 @@
          <div class="card-body">
              <h5 class="card-title">Daftar Pesanan Saya</h5>
              <div class="table-container border">
-             <table>
+               <table>
+                  <thead>
+                    <tr>
+                      <th scope="col" class="text-center">No</th>
+                      <th scope="col" class="text-center">Nama</th>
+                      <th scope="col" class="text-center">Tujuan</th>
+                      <th scope="col" class="text-center">Status</th>
+                      <th scope="col" class="text-center">Action</th>
+                    </tr>
+                  </thead>
+                  
+                  <tbody>
+                     @php($no=1)
+                    @if(count($pesanan) > 0)
+                      @foreach($pesanan as $item)
+                        <tr>
+                          <td scope="row" class="text-center">{{ $no++ }}</td>
+                          <td class="text-center">{{ $item->nama }}</td>
+                          <td class="text-center">{{ $item->tujuan }}</td>
+                          <td class="text-center">{{ $item->status }}</td>
+                          <td class="text-center">
+                           <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modal{{ $item->id }}">
+                              Detail
+                            </button>
+                          </td>
+                        </tr>
+                
+                        <!-- Modal -->
+                        <div class="modal fade" id="modal{{ $item->id }}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                           <div class="modal-dialog">
+                           <div class="modal-content">
+                              <div class="modal-header">
+                                 <h1 class="modal-title fs-5" id="exampleModalLabel">Detail Pesanan</h1>
+                                 {{-- <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button> --}}
+                              </div>
+                              <div class="modal-body">
+                                 <p>Nama : {{ $item->nama }}</p>
+                                 <p>Kontak : {{ $item->kontak }}</p>
+                                 <p>Lokasi Penjemputan : {{ $item->lokasi }}</p>
+                                 <p>Tujuan : {{ $item->tujuan }}</p>
+                                 <p>Waktu : {{ $item->waktu }}</p>
+                                 <p>Kendaraan : {{ $item->kendaraan }}</p>
+                                 <p>Jenis Jasa : {{ $item->jenis }}</p>
+                                 @if(isset($item->tanggal_pulang))
+                                    <p>Tanggal Pulang : {{ $item->tanggal_pulang }}</p>
+                                 @endif
+                                 @if(isset($item->keterangan))
+                                    <p>Keterangan : {{ $item->keterangan }}</p>
+                                 @else
+                                    <p>Keterangan : -</p>
+                                 @endif
+                                 <p>Status : {{ $item->status }}</p>
+                              </div>
+                              <div class="modal-footer">
+                                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                 <a class="btn btn-primary" a href="{{ route('prosesPesanan', ['id' => base64_encode($item->id)]) }}">Edit</a>
+                              </div>
+                           </div>
+                           </div>
+                        </div>
+                      @endforeach
+                    @else
+                      <tr>
+                        <td colspan="3" class="text-center">Tidak ada pesanan</td>
+                      </tr>
+                    @endif
+                  </tbody>
+                </table>
+             {{-- <table>
                 <thead>
                    <tr>
                     <th scope="col" class="text-center">No</th>
@@ -104,7 +172,7 @@
                    </tr>
                    @endif
                 </tbody>
-             </table>
+             </table> --}}
             </div>
          </div>
       </div>
